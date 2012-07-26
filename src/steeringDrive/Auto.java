c50af64b9748fbbf68a35ad64a21caf2669c4092
gillesB <baatzgilles@googlemail.com>
2012-07-26 16:30:03 +0200

    strukturiert

--- src/steeringDrive/Auto.java
+++ src/steeringDrive/Auto.java
@@ -11,81 +11,36 @@ import lejos.robotics.navigation.DifferentialPilot;
 
 public class Auto {
 
-	double f = 20.5; // Abstand von Hinterachse zur Wagenfront
-	double w = 13.5; // Breite des Autos
-	double R = 47.75; // Wendekreis
-	double b = 3.0; // Abstand von Hinterachse zum Wagenende
+	private double f = 20.5; // Abstand von Hinterachse zur Wagenfront
+	private double w = 13.5; // Breite des Autos
+	private double R = 47.75; // Wendekreis
+	private double b = 3.0; // Abstand von Hinterachse zum Wagenende
 	// r der Radius des kleinsten Kreises, den der Automittelpunkt, beschreiben
 	// kann
-	double r = (Math.sqrt(Math.pow(R, 2) - Math.pow(f, 2)) - w / 2); // sqrt(R^2-f^2)-w/2
-	double p = 15.00;
-	double alpha = Math.acos((2 * r - w - p) / (2 * r));
-	double g = Math.sqrt(2 * r * w + Math.pow(f, 2)) + b; // minLaengeParkluecke
-	double kreisbogen = r * alpha;
+	private double r = (Math.sqrt(Math.pow(R, 2) - Math.pow(f, 2)) - w / 2); // sqrt(R^2-f^2)-w/2
+	private double p = 15.00;
+	private double alpha = Math.acos((2 * r - w - p) / (2 * r));
+	private double g = Math.sqrt(2 * r * w + Math.pow(f, 2)) + b; // minLaengeParkluecke
+	private double kreisbogen = r * alpha;
 
-	public final float WHEEL_DIAMETER = 5.00f;
-	public final float TRACK_WIDTH = 13.50f;
-	public final boolean REVERSE = true;
-	private DifferentialPilot pilot = new DifferentialPilot(WHEEL_DIAMETER,
-			TRACK_WIDTH, Motor.A, Motor.C, REVERSE);
+	private float reifenDurchmesser = 5.00f;
+	private float spurweite = 13.50f;
+	private boolean rueckwaertslaufend = true;
+	private DifferentialPilot pilot = new DifferentialPilot(reifenDurchmesser,
+			spurweite, Motor.A, Motor.C, rueckwaertslaufend);
 
-	public final double DISTANZ_SENSOR_HINTERACHSE = 9.00;
-	public final float MAXIMALE_PARKLUECKE = 50;
+	private double distanzSensorHinterachse = 9.00;
+	private float maximaleParkluecke = 70;
 
 	private EOPD eopdRechts = new EOPD(SensorPort.S1, true);
 	private EOPD eopdHinten = new EOPD(SensorPort.S2, true);
+	
+	private float gemesseneStreckeStart;
 
 	public Auto() {
 		super();
 		this.pilot.setTravelSpeed(15); // cm/s
 		Sound.setVolume(50);
-	}
-
-	public void rueckwaertsEinparken(float parkluecke) {
-		RadEinschlag.einschlag_prozent(100);
-		System.out.println("r: " + r);
-		System.out.println("alpha: " + alpha);
-		System.out.println("kreisbogen: " + kreisbogen);
-		System.out.println("minLaengeParkluecke: " + g);
-		pilot.travel(-kreisbogen, true);
-		this.achteAufGegenstandHinten();
-		RadEinschlag.einschlag_prozent(-100);
-		pilot.travel(-kreisbogen, true);
-		this.achteAufGegenstandHinten();
-		RadEinschlag.einschlag_prozent(0);
-		nachVorneRangieren(parkluecke);
-	}
-
-	public void achteAufGegenstandHinten() {
-		while (pilot.isMoving()) {
-			if (gegenstandHinten()) {
-				pilot.stop();
-				Sound.buzz();
-			}
-		}
-	}
-
-	public void rausfahren() {
-		this.hintenAnGegenstandFahren();
-		RadEinschlag.einschlag_prozent(-100);
-		System.out.println("r: " + r);
-		System.out.println("alpha: " + alpha);
-		System.out.println("kreisbogen: " + kreisbogen);
-		System.out.println("minLaengeParkluecke: " + g);
-		pilot.travel(kreisbogen * 0.80);
-		RadEinschlag.einschlag_prozent(100);
-		pilot.travel(kreisbogen);
-	}
-
-	float gemesseneDistanzStart;
-
-	public void messeStreckeStart() {
-		gemesseneDistanzStart = pilot.getMovement().getDistanceTraveled();
-	}
-
-	public float messeStrecke() {
-		return pilot.getMovement().getDistanceTraveled()
-				- gemesseneDistanzStart;
 	}
 
 	public void passiereErstenGegenstand() {
@@ -103,43 +58,15 @@ public class Auto {
 	public float fahreBisZweiterGegenstandUndErmittleParkluecke() {
 		float parkluecke = vermesseParkluecke();
 
-		if (parkluecke < this.MAXIMALE_PARKLUECKE) {
+		if (parkluecke < this.maximaleParkluecke) {
 			while (this.messeStrecke() < parkluecke
-					+ this.DISTANZ_SENSOR_HINTERACHSE)
+					+ this.distanzSensorHinterachse)
 				;
 		}
 
 		pilot.stop();
 		return parkluecke;
 
-	}
-
-	public float vermesseParkluecke() {
-		System.out.println("Starte Vermessung Parkluecke");
-		this.messeStreckeStart();
-		while (!this.gegenstandRechts()
-				&& this.messeStrecke() < this.MAXIMALE_PARKLUECKE)
-			;
-		float parkluecke = this.messeStrecke();
-		System.out.println("Vermessung Parkluecke abgeschlossen");
-		Sound.beep();
-		return parkluecke;
-	}
-
-	public boolean gegenstandRechts() {
-		if (eopdRechts.readRawValue() < 1000) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public boolean gegenstandHinten() {
-		if (eopdHinten.readRawValue() < 800) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 
 	public boolean kannEinparken(float parkluecke) {
@@ -153,24 +80,30 @@ public class Auto {
 		return true;
 	}
 
-	public void hintenAnGegenstandFahren() {
-		this.pilot.backward();
-		while (!this.gegenstandHinten())
-			;
-		Sound.beep();
-		this.pilot.stop();
+	public void rueckwaertsEinparken(float parkluecke) {
+		RadEinschlag.einschlag_prozent(100);
+		pilot.travel(-kreisbogen, true);
+		this.achteAufGegenstandHinten();
+		RadEinschlag.einschlag_prozent(-100);
+		pilot.travel(-kreisbogen, true);
+		this.achteAufGegenstandHinten();
+		// Ausgleich einer Unpraezision der Lenkung
+		int offset = 10;
+		RadEinschlag.einschlag_prozent(0 + offset);
+
+		nachVorneRangieren(parkluecke);
 	}
 
-	public void nachVorneRangieren(float parkluecke) {
-		hintenAnGegenstandFahren();
-		System.out.println("Parkluecke: " + parkluecke);
-		this.pilot.travel((parkluecke - this.f) / 2);
+	public void rausfahren() {
+		this.hintenAnGegenstandFahren();
+		RadEinschlag.einschlag_prozent(-100);
+		pilot.travel(kreisbogen * 0.80);
+		RadEinschlag.einschlag_prozent(100);
+		pilot.travel(kreisbogen);
+		RadEinschlag.einschlag_prozent(0);
 	}
 
 	public void schlussfahrt() {
-
-		RadEinschlag.einschlag_prozent(0);
-
 		Sound.setVolume(100);
 		File soundFile = new File("smw_course_clear.wav");
 		System.out.println(soundFile.length());
@@ -178,6 +111,66 @@ public class Auto {
 
 		this.pilot.travel(50);
 
+	}
+
+	private void achteAufGegenstandHinten() {
+		while (pilot.isMoving()) {
+			if (gegenstandHinten()) {
+				pilot.stop();
+				Sound.buzz();
+			}
+		}
+	}
+
+	private void messeStreckeStart() {
+		gemesseneStreckeStart = pilot.getMovement().getDistanceTraveled();
+	}
+
+	private float messeStrecke() {
+		return pilot.getMovement().getDistanceTraveled()
+				- gemesseneStreckeStart;
+	}
+
+	private float vermesseParkluecke() {
+		System.out.println("Starte Vermessung Parkluecke");
+		this.messeStreckeStart();
+		while (!this.gegenstandRechts()
+				&& this.messeStrecke() < this.maximaleParkluecke)
+			;
+		float parkluecke = this.messeStrecke();
+		System.out.println("Vermessung Parkluecke abgeschlossen");
+		Sound.beep();
+		return parkluecke;
+	}
+
+	private boolean gegenstandRechts() {
+		if (eopdRechts.readRawValue() < 1000) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private boolean gegenstandHinten() {
+		if (eopdHinten.readRawValue() < 800) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void hintenAnGegenstandFahren() {
+		this.pilot.backward();
+		while (!this.gegenstandHinten())
+			;
+		Sound.beep();
+		this.pilot.stop();
+	}
+
+	private void nachVorneRangieren(float parkluecke) {
+		hintenAnGegenstandFahren();
+		System.out.println("Parkluecke: " + parkluecke);
+		this.pilot.travel((parkluecke - this.f) / 2);
 	}
 
 }
